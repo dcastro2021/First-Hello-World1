380feedf0df9dfc77436e18fcf5383294707a6b8
David A Castro <83044518+dcastro2021@users.noreply.github.com>
2021-04-27 21:13:11 -0500

    second push

--- HelloWorld1.py
+++ HelloWorld1.py
@@ -1,1 +1,15 @@
 print("Hello World")
+print("So it begins")
+
+print("-------")
+print("  _ _  ")
+print("- o o -")
+print("-  0  -")
+print("-------")
+
+print(3 x 5)
+print("3 x 5")
+
+print(0x0)
+
+print("So it ends")
